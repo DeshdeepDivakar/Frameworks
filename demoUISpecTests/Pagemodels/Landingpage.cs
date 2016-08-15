09d29fbc94a45a33cd558248cd23c8ebadd5cd11
Deshdeep <deshdeepdivakar@gmail.com>
2016-08-15 13:18:18 +1000

    demoframework

---
+++ demoUISpecTests/Pagemodels/Landingpage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace demoUISpecTests.Pagemodels
+{
+    public class LandingPage : BasePage
+    {
+        public LandingPage(IWebDriver webdriver):base(webdriver)
+        {
+            PageFactory.InitElements(webdriver, this);
+        }
+
+        [FindsBy(How=How.XPath,Using = "//div[@class='card-mask']/div[@class='one-google']/p//span//a[contains(.,'Create account')]")]
+        public IWebElement createAccount;       
+
+
+
+        public void createAcc()
+        {
+            createAccount.Click();
+        }
+
+    }
+}
